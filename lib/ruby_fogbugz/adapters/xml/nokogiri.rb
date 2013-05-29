7db0b3268ee130e205cca55205235fedfeeb7b6d
Spencer Bean <spencer.bean@moneydesktop.com>
2013-05-28 18:42:13 -0600

    Dependency for nokogiri

--- lib/ruby_fogbugz/adapters/xml/nokogiri.rb
+++ lib/ruby_fogbugz/adapters/xml/nokogiri.rb
@@ -1,11 +1,12 @@
 require 'nokogiri'
+require 'open-uri'
 
 module Fogbugz
   module Adapter
     module XML
       class Nokogiri
         def self.parse(xml)
-          Nokogiri::XML::Document.parse(xml)["response"]
+          Nokogiri::XML::Document.parse(xml)["response"] { |config| config.nonet }
         end
       end
     end
